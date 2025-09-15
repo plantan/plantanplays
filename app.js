8cc68e7018962fc54c989f9a9dda4bc8806497a3
plantan <plantan@fastmail.com>
2025-09-15 21:06:35 +0200

    WIP loading img urls from video game database. borken code.

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ createApp({
         function loadCSV() {
             fetch("games-finished.csv")
             .then(response => response.text())
-            .then(csv => Papa.parse(csv, { complete(parseResult) { parsedCSV.value = parseResult.data } }))
+            .then(csv => Papa.parse(csv, { complete(parseResult) 
+                { 
+                    parseResult.data.shift();
+                    parsedCSV.value = parseResult.data;
+                }}))
             .catch(error => console.error("Error fetching games list:", error));
 
             fetch("game-covers.json")
@@ -16,6 +20,8 @@ createApp({
             .then(json => images.value = json)
             .catch(error => console.error("Error fetching images:", error));
         }
+
+        loadCSV();
 
         return {
             loadCSV,
--- update-game-covers.ts
+++ update-game-covers.ts
@@ -1,26 +1,43 @@
 import Papa from 'papaparse';
+import Bun from 'bun';
 
 const games = Bun.file("games-finished.csv");
 const text = await games.text();
-const apiKey = process.argv.slice(2)[0];
+const id = process.argv.slice(2)[0];
+const secret = process.argv.slice(2)[1];
 
-Papa.parse(text, { header: true, complete(csv) {
-    const rawgids = new Array<string>();
-    for (const row of csv.data) {
-        const rawgid = row["RAWGID"];
-        if(rawgid != undefined && rawgid.length > 0) {
-            rawgids.push(rawgid);
-        }
-    }
+console.log(id);
+console.log(secret);
+
+const response = await fetch(new Request(`https://id.twitch.tv/oauth2/token?client_id=${id}&client_secret=${secret}&grant_type=client_credentials`, { method: "POST", }));
+const jsonResponse = await response.json();
+console.log(jsonResponse["access_token"]);
+console.log(jsonResponse["expires_in"]);
+console.log(jsonResponse["token_type"]);
+
+
+//   "access_token": "access12345token",
+//   "expires_in": 5587808,
+//   "token_type": "bearer"
+
+// Papa.parse(text, { header: true, complete(csv) {
+//     const rawgids = new Array<string>();
+//     for (const row of csv.data) {
+//         const rawgid = row["RAWGID"];
+//         if(rawgid != undefined && rawgid.length > 0) {
+//             rawgids.push(rawgid);
+//         }
+//     }
     
-    const map = new Map<string, string>();
-    Promise.all(rawgids.map(async (rawgid) => {
-        const res = await fetch(`https://api.rawg.io/api/games/${rawgid}?key=${apiKey}`);
-        const json = await res.json();
-        map.set(rawgid, json.background_image);
-    })).then(() => {
-        const writer = Bun.file("game-covers.json").writer();
-        writer.write(JSON.stringify(Object.fromEntries(map)));
-        writer.end()
-    });
-}});
+//     const map = new Map<string, string>();
+//     Promise.all(rawgids.map(async (rawgid) => {
+//         const res = await fetch(`https://api.rawg.io/api/games/${rawgid}?key=${apiKey}`);
+//         const json = await res.json();
+//         console.log(json)
+//         map.set(rawgid, json.background_image);
+//     })).then(() => {
+//         const writer = Bun.file("game-covers.json").writer();
+//         writer.write(JSON.stringify(Object.fromEntries(map)));
+//         writer.end()
+//     });
+// }});
